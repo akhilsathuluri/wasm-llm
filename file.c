e0835c6dbacd4eb82ede54f0330939c5f2f5f0d5
laniakea <akhilsathuluri@gmail.com>
2023-11-02 19:12:33 +0100

    works for hello world!

---
+++ file.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+ int hello(){//print hello world
+ printf("hello world\n");
+ return 0;
+ }
+ int main(){
+ hello();
+ return 0;
+ }
+
--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 
 import streamlit as st
 import pywasm
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from wasmtime import *
 
-st.title("Welcome to auto-web")
+# st.title("Welcome to auto-web")
 
 # create a sidebar to input a text field
 with st.sidebar:
     st.write("Enter your message below:")
     message = st.text_input("Message", "Type here...")
     if st.button("Send"):
-        # send prompt to create and compile a wasm code
-
         st.write("Message sent!")
 
 # Use this to have a defined feature
@@ -34,18 +35,66 @@ add_button_offline(st)
 # add_button_online(st)
 
 # model inference
-from transformers import AutoTokenizer, AutoModelForCausalLM
-tokenizer = AutoTokenizer.from_pretrained("./models/codegen25-7b-instruct", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("./models/codegen25-7b-instruct")
+@st.cache_resource
+def load_models():
+    # This function will only be run the first time it's called
+    tokenizer = AutoTokenizer.from_pretrained("./models/codegen25-7b-instruct", trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained("./models/codegen25-7b-instruct")
+    return tokenizer, model
 
-def format(prefix, suffix):
-  return prefix + "<mask_1>" + suffix + "<|endoftext|>" + "<sep>" + "<mask_1>"
+tokenizer, model = load_models()
 
-prefix = "def hello_world():\n    "
-suffix = "    return name"
-text = format(prefix, suffix)
-input_ids = tokenizer(text, return_tensors="pt").input_ids
-generated_ids = model.generate(input_ids, max_length=128)
-st.write(tokenizer.decode(generated_ids[0], skip_special_tokens=False)[len(text):])
+# Use this to load a feature compiled in wasm
+def add_button_online(st):
+    st.write("This is an online button")
+    if st.button("Click me", "online"):
+      print("received prompt .... ")
+      # read the input message
+      comment = message
+      st.write("received prompt....")
 
+      # inputs = tokenizer("#include <iostream> \n using namespace std;\n int hello(){//print hello world\n", return_tensors="pt")
 
+      # pad the prompt
+      prefix = "#include <stdio.h> \n int hello(){//"
+      suffix = "return 0;}"
+      # prompt = prefix + comment + "\n" + "<|endoftext|>" + "<sep>" + "<mask_1>" + suffix 
+      prompt = prefix + comment + "\n" 
+      st.write(prompt)
+
+      inputs = tokenizer(prompt, return_tensors="pt")
+      # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+      sample = model.generate(**inputs, max_length=128)
+      # sample = model.generate(input_ids, max_length=128)
+      print(tokenizer.decode(sample[0]))
+      output = tokenizer.decode(sample[0])
+
+      print("printing the code onto drive .....")
+      #print output to a file
+      with open('file.c', 'w') as f:
+        f.write(output.split("###")[0])
+      
+      print("compiling the code ....")
+      # compile the file
+      os.system("""emcc -Os -s STANDALONE_WASM -s EXPORTED_FUNCTIONS="['_hello']" --no-entry file.c -o file.wasm""")
+
+      print("executing the code ....")
+      # runtime = pywasm.load("./file.wasm")
+      # r = runtime.exec('hello')
+      engine = Engine()
+      store = Store(engine)
+      module = Module.from_file(engine, "./file.wasm")
+      linker = Linker(engine)
+      linker.define_wasi()
+      wasi = WasiConfig()
+      wasi.inherit_stdout()
+      store.set_wasi(wasi)
+      instance = linker.instantiate(store, module)
+      exports = instance.exports(store)
+      
+      exports["hello"](store)
+
+      # st.write(r)
+      st.write("Executed, check terminal!")
+
+add_button_online(st)
